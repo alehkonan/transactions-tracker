ede3dafce69754eea0a846210ef23d8cb4a59d28
k <k@email.vi>
2026-07-19 17:27:49 +0400

    init

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import tailwindcss from "@tailwindcss/vite";
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
+  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  server: {
+    port: 5432,
+  },
+  preview: {
+    port: 5432,
+  },
+});
