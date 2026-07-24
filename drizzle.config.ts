ff7adb80a6058968d9a3eea5fd2938d0aaca2b36
Oleg Konan <olegkonanff@gmail.com>
2026-07-24 13:38:48 +0400

    chore: use local db for dev

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from "drizzle-kit";
 
+process.loadEnvFile(".env.local");
+
+if (!process.env.DATABASE_URL) {
+  console.log("DATABASE_URL is not set in env");
+  process.exit(1);
+}
+
 export default defineConfig({
+  dialect: "postgresql",
   schema: "./src/drizzle/schema.ts",
   out: "./src/drizzle/migrations",
-  dialect: "postgresql",
-  dbCredentials: { url: process.env.DATABASE_URL! },
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
 });
--- src/utils/transaction.functions.ts
+++ src/utils/transaction.functions.ts
@@ -2,6 +2,8 @@ import { createServerFn } from "@tanstack/react-start";
 import { transactions } from "~/drizzle/schema";
 import { getDb } from "~/lib/db.server";
 
-export const getTransactions = createServerFn({ method: "GET" }).handler(async () => {
+export const getTransactions = createServerFn({
+  method: "GET",
+}).handler(async () => {
   return getDb().select().from(transactions);
 });
