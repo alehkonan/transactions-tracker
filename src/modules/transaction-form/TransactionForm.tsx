c5d8c064a2a069511be94bdc9c9d65a064292fbe
Aleh <olegkonanff@gmail.com>
2026-08-30 13:38:57 +0400

    Use decimal input modes for monetary fields

--- src/modules/transaction-form/TransactionForm.tsx
+++ src/modules/transaction-form/TransactionForm.tsx
@@ -196,7 +196,7 @@ export function TransactionForm({ accounts, categories, transaction }: Props) {
                 label="Amount"
                 rules={{ required: "Amount is required." }}
                 type="number"
-                inputMode="numeric"
+                inputMode="decimal"
                 step="0.01"
                 min="0.01"
                 className="w-full"
@@ -226,7 +226,7 @@ export function TransactionForm({ accounts, categories, transaction }: Props) {
                 label="Amount"
                 rules={{ required: "Amount is required." }}
                 type="number"
-                inputMode="numeric"
+                inputMode="decimal"
                 step="0.01"
                 min="0.01"
                 className="w-full"
@@ -255,7 +255,7 @@ export function TransactionForm({ accounts, categories, transaction }: Props) {
             label="Amount"
             rules={{ required: "Amount is required." }}
             type="number"
-            inputMode="numeric"
+            inputMode="decimal"
             step="0.01"
             min="0.01"
             className="w-full"
